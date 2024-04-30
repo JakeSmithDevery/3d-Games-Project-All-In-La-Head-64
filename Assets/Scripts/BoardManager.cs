58bbbbf6e6301bacf4e04bc0fa9a62d2cc744981
Jake Smith <S00234729@atu.ie>
2024-04-30 16:44:34 +0100

    map generator updated

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,16 +3,21 @@ using System;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 using Unity.AI.Navigation;
+using UnityEngine.UIElements;
 
 public class BoardManager : MonoBehaviour
 {
-	public int columns = 8;//Number of columns in our game board.
-	public int rows = 8;//Number of rows in our game board.
+	int columns;//Number of columns in our game board.
+	int rows;//Number of rows in our game board.
+	public int Enemiesleft;
 	public Count wallCount = new Count(5, 9);//Lower and upper limit for our random number of walls per level.
-	public BoardPieceData exit;//Prefab to spawn for exit.
+    public Count EnemyCount = new Count(5, 9);
+    public BoardPieceData exit;//Prefab to spawn for exit.
 	public BoardPieceData[] floorTiles;//Array of floor prefabs.
 	public BoardPieceData[] wallTiles;//Array of wall prefabs.                            
 	public BoardPieceData[] outerWallTiles;//Array of outer tile prefabs.
+
+	
 
 	private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
 	private List<Vector3> gridPositions = new List<Vector3>();  //A list of possible locations to place tiles.
@@ -25,8 +30,9 @@ public class BoardManager : MonoBehaviour
     //Clears our list gridPositions and prepares it to generate a new board.
     void InitialiseList()
 	{
-		//Clear our list gridPositions.
-		gridPositions.Clear();
+
+        //Clear our list gridPositions.
+        gridPositions.Clear();
 
 		//Loop through x axis (columns).
 		for (int x = 1; x < columns - 1; x++)
@@ -102,15 +108,45 @@ public class BoardManager : MonoBehaviour
 			GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)].Prefab;
 
 			//Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
-			Instantiate(tileChoice, randomPosition, Quaternion.identity);
+			
+			
+				Instantiate(tileChoice, randomPosition, Quaternion.identity);
+			
+			
 		}
 	}
 
-	//SetupScene initializes our level and calls the previous functions to lay out the game board
-	public void SetupScene()
+    void LayoutEnemiesAtRandom(BoardPieceData[] tileArray, int minimum, int maximum)
+    {
+        //Choose a random number of objects to instantiate within the minimum and maximum limits
+        Enemiesleft = Random.Range(minimum, maximum + 1);
+
+        //Instantiate objects until the randomly chosen limit objectCount is reached
+        for (int i = 0; i < Enemiesleft; i++)
+        {
+            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+            Vector3 randomPosition = RandomPosition();
+
+            //Choose a random tile from tileArray and assign it to tileChoice
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)].Prefab;
+
+            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+
+
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+
+
+        }
+    }
+
+    //SetupScene initializes our level and calls the previous functions to lay out the game board
+    public void SetupScene()
 	{
-		//Creates the outer walls and floor.
-		BoardSetup();
+        columns = Random.Range(10, 35);
+        rows = Random.Range(10, 35);
+
+        //Creates the outer walls and floor.
+        BoardSetup();
 
 		//Reset our list of gridpositions.
 		InitialiseList();
@@ -119,7 +155,16 @@ public class BoardManager : MonoBehaviour
 		LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
 
 		//Instantiate the exit tile in the middle of the board
-		Instantiate(exit.Prefab, new Vector3(columns /2, 0f, rows /2), Quaternion.identity);
+		if (Enemiesleft == 0)
+		{
+			Instantiate(exit.Prefab, new Vector3((columns*3) / 2 , 0f, (rows*3) / 2), Quaternion.identity);
+        }
+		var hits = Physics.OverlapSphere(new Vector3((columns * 3) / 2, 1f, (rows * 3) / 2), 0.1f);
+		foreach (var collider in hits)
+			if (!collider.CompareTag("Player"))
+			{
+				Destroy(collider.gameObject);
+			}
 
 		GetComponent<NavMeshSurface>().BuildNavMesh();
 	}
