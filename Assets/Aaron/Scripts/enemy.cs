28350649d0eac80183e09792f11188f1b4b0b25d
Aaron Mc Kenna Lappin <123363738+S00233610@users.noreply.github.com>
2024-05-14 19:16:16 +0100

    Buh1
    enemy now added yippee

---
+++ Assets/Aaron/Scripts/enemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int Damage = 10;
+    public int Health = 100;
+    public int MaxHealth = 100;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleCollision(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollision(collision.gameObject);
+    }
+
+    public void AddHealth(int amount)
+    {
+        Health = Health + amount;
+
+        if (Health > MaxHealth)
+        {
+            Health = MaxHealth;
+        }
+    }
+
+    public void SubtractHealth(int amount)
+    {
+        Health = Health - amount;
+        if (Health <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    public virtual void OnDeath()
+    {
+
+    }
+
+    public virtual void HandleCollision(GameObject otherObject)
+    {
+
+    }
+}
