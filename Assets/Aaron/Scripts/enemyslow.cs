726cb78c75a0cf46d397d5a5e1e69a13d6a22495
Aaron Mc Kenna Lappin <123363738+S00233610@users.noreply.github.com>
2024-05-15 19:33:45 +0100

    buh 2
    hopefully the bulers are working (if not yippee)

--- Assets/Aaron/Scripts/enemyslow.cs
+++ Assets/Aaron/Scripts/enemyslow.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class enemyslow : enemy
 {
-    
+    public GameObject Bullet;
+    public Transform BulletSpawner;
+
+    private float bulletTime;
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(Bullet, BulletSpawner.transform.position, BulletSpawner.transform.rotation) as GameObject;
+    }
 }
